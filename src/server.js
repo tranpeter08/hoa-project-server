89ff3544f997b529665f73deb03331af6ea9e91f
Peter Tran <tran.peter08@gmail.com>
2019-09-11 00:07:51 -0700

    added knex instance

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 'use strict';
 const app = require('./app');
-const {PORT} = require('./config');
+const knex = require('knex');
+const {PORT, DB_URL} = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL
+});
+
+console.log('Connected to database');
+
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`App is listening on port: ${PORT}`);
