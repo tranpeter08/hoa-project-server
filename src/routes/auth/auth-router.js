0ee8fe7032adcbf379d36f5824f1bca93e193322
Peter Tran <tran.peter08@gmail.com>
2019-09-13 03:34:19 -0700

    removed local strategy

--- src/routes/auth/auth-router.js
+++ src/routes/auth/auth-router.js
@@ -1,10 +1,46 @@
 const express = require('express');
-const 
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const {JWT_SECRET, JWT_EXPIRY} = require('../../config');
+const app = require('../../app');
+
+const UserService = require('../../services/UserService');
 
 const authRouter = express.Router();
 
-authRouter.post('/login', (req, res) => {
-  const {username, password} = req.body;  
+authRouter.post('/login', async (req, res, next) => {
+  const db = req.app.get('db');
+  const {username, password} = req.body;
+
+  try {
+    const results = await UserService.getUserLogin(db, username);
+    const user = results[0];
+
+    if (!user) {
+      return res.status(400).json({message: 'Incorrect username'});
+    }
+
+    const isValid = await bcrypt.compare(password, user.user_pw);
+
+    if (!isValid) {
+      return res.status(400).json({message: 'Incorrect password'});
+    }
+
+    delete user.user_pw;
+
+    const jwtOpts = {
+      algorithm: 'HS256',
+      expiresIn: JWT_EXPIRY,
+    }
+
+    const authToken = jwt.sign(user, JWT_SECRET, jwtOpts);
+
+    res.status(200).json({authToken});
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = authRouter;
