3fd8f7d05f4c2e88864495d6fbea80284b19a4ae
Peter Tran <tran.peter08@gmail.com>
2019-09-12 22:34:27 -0700

    added database test url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT : process.env.PORT || '8080',
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://staff:@localhost/hoa_project'
+  DB_URL: process.env.DATABASE_URL || 'postgresql://staff:@localhost/hoa_project',
+  DB_URL: process.env.DATABASE_URL_TEST || 'postgresql://staff:@localhost/hoa_project_test'
   // add database
 }
