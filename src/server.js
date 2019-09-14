3e392e719da1a6768072d107bba6133bcded05fd
Peter Tran <tran.peter08@gmail.com>
2019-09-14 14:40:37 -0700

    moved passport import

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@
 const app = require('./app');
 const knex = require('knex');
 const {PORT, DB_URL} = require('./config');
-const passport = require('passport');
-const {localStrat, jwtStrat} = require('./routes/auth/strategies');
 
 const db = knex({
   client: 'pg',
@@ -13,8 +11,6 @@ const db = knex({
 console.log('Connected to database');
 
 app.set('db', db);
-
-passport.use(jwtStrat);
 
 app.listen(PORT, () => {
   console.log(`App is listening on port: ${PORT}`);
